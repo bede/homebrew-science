8ae269745e5b4512c932d54d2c95f78f745856fd
Bede Constantinides <bedeabc@gmail.com>
2016-01-15 16:21:17 +0000

    spades.rb: build SPAdes 3.6.2 with GCC >=4.7 for OpenMP support
    spades.rb previously built with clang, which prevented multithreading.

--- spades.rb
+++ spades.rb
@@ -1,6 +1,12 @@
 class Spades < Formula
   desc "SPAdes: de novo genome assembly"
   homepage "http://bioinf.spbau.ru/spades/"
+  bottle do
+    cellar :any
+    sha256 "9d69a98a442701f818df56325beeb1ddc8457da5932da172706becf18f6d3fed" => :yosemite
+    sha256 "89ff010f4542bb2f63144c642a125fdedee8367a14410bfd3b30c6b27caeb1c9" => :mavericks
+    sha256 "834d20f5a8b812cdb8c1e2b9c36a8baf35b5f6e5d7f3af910350713a3357ae5e" => :mountain_lion
+  end
 
   # tag "bioinformatics"
   # doi "10.1089/cmb.2012.0021"
@@ -9,6 +15,12 @@ class Spades < Formula
   sha256 "20897e2707623ee1033d3c88fefc42771fa4bbaafaf0f95642991d83b361eb5f"
 
   depends_on "cmake" => :build
+
+  needs :openmp
+
+  fails_with :gcc => "4.7" do
+    cause "Compiling SPAdes requires GCC >= 4.7 for OpenMP 3.1 support"
+  end
 
   def install
     mkdir "src/build" do
