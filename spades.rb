661db739a566e1aede0c2caafc30fdcaf39b1323
Bede Constantinides <bedeabc@gmail.com>
2016-01-15 15:38:34 +0000

    spades.rb: SPAdes 3.6.2

--- spades.rb
+++ spades.rb
@@ -1,18 +1,12 @@
 class Spades < Formula
   desc "SPAdes: de novo genome assembly"
   homepage "http://bioinf.spbau.ru/spades/"
-  bottle do
-    cellar :any
-    sha256 "9d69a98a442701f818df56325beeb1ddc8457da5932da172706becf18f6d3fed" => :yosemite
-    sha256 "89ff010f4542bb2f63144c642a125fdedee8367a14410bfd3b30c6b27caeb1c9" => :mavericks
-    sha256 "834d20f5a8b812cdb8c1e2b9c36a8baf35b5f6e5d7f3af910350713a3357ae5e" => :mountain_lion
-  end
 
   # tag "bioinformatics"
   # doi "10.1089/cmb.2012.0021"
 
-  url "http://spades.bioinf.spbau.ru/release3.6.0/SPAdes-3.6.0.tar.gz"
-  sha256 "6c04e55d068ad3e5b44c862cb031d41fcd433ab97e5dedadebcc5f785cde13a9"
+  url "http://spades.bioinf.spbau.ru/release3.6.2/SPAdes-3.6.2.tar.gz"
+  sha256 "20897e2707623ee1033d3c88fefc42771fa4bbaafaf0f95642991d83b361eb5f"
 
   depends_on "cmake" => :build
 
